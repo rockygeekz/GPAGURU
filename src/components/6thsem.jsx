45c9c20c04ff023da44ba6e65211f52b5fef7bb2
Nikhil AL <nikhilal.2703@gmail.com>
2023-10-26 23:03:19 +0530

    cgpa and sgpa done but ui is not good

---
+++ src/components/6thsem.jsx
@@ -0,0 +1,154 @@
+import React, { useState } from "react";
+import CgpaResult from './cgpasheet.jsx';
+
+
+const Sem6 = () =>{
+
+    const [inputText, setinputText] = useState(
+        {
+            sem1: null,
+            sem2: null,
+            sem3: null,
+            sem4: null,
+            sem5: null,
+            sem6: null,
+          });
+    
+    
+      const modchange = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+        setinputText((old) => {
+          return { ...old, [name]: Number(value) };
+        });
+      };
+    
+      const [clicked,setclick]=useState(false);
+    
+      const modSubmit = (e) => {
+        e.preventDefault();
+        setclick(true)
+      };
+    
+      const reset = (e) => {
+        setinputText( 
+          {
+            sem1: null,
+            sem2: null,
+            sem3: null,
+            sem4: null,
+            sem5: null,
+            sem6: null,
+          }
+        )
+        setclick(false)
+      }
+
+
+    return (
+        <div className="absolute top-0 left-0 min-h-fit min-w-full container mt-10">
+        <form action="" onSubmit={modSubmit}>
+          <div className="inputs ">
+            <div className="left flex flex-col gap-y-8 text-white min-h-screen min-w-full p-9  font-mono">
+              <label htmlFor="">
+                1st semister 
+                <input
+                  type="number"
+                  name="sem1"
+                  value={inputText.sem1}
+                  className="m-3 rounded-lg text-black"
+                  placeholder="Sgpa"
+                  onChange={modchange}
+                />
+              </label>
+
+              <label htmlFor="">
+                2nd semister
+                <input
+                  type="number"
+                  name="sem2"
+                  value={inputText.sem2}
+                  className="m-3 rounded-lg text-black"
+                  placeholder="Sgpa"
+                  onChange={modchange}
+                />
+              </label>
+
+
+              <label htmlFor="">
+                3rd semister
+                <input
+                  type="number"
+                  name="sem3"
+                  value={inputText.sem3}
+                  className="m-3 rounded-lg text-black"
+                  placeholder="Sgpa"
+                  onChange={modchange}
+                />
+              </label>
+
+
+              <label htmlFor="">
+                4th semister
+                <input
+                  type="number"
+                  name="sem4"
+                  value={inputText.sem4}
+                  className="m-3 rounded-lg text-black"
+                  placeholder="Sgpa"
+                  onChange={modchange}
+                />
+              </label>
+
+
+              <label htmlFor="">
+                5th semister
+                <input
+                  type="number"
+                  name="sem5"
+                  value={inputText.sem5}
+                  className="m-3 rounded-lg text-black"
+                  placeholder="Sgpa"
+                  onChange={modchange}
+                />
+              </label>
+
+
+              <label htmlFor="">
+                6th semister
+                <input
+                  type="number"
+                  name="sem6"
+                  value={inputText.sem6}
+                  className="m-3 rounded-lg text-black"
+                  placeholder="Sgpa"
+                  onChange={modchange}
+                />
+              </label>
+
+              <div className="last-btn flex justify-center items-center gap-3  ">
+              <button
+                  type="submit"
+                  onClick={modSubmit}
+                  className=" w-24  flex justify-center rounded bg-neutral-50 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]"
+                >
+                  SUBMIT
+                </button>
+                <button
+                  type="reset"
+                  className=" w-24  flex justify-center rounded bg-neutral-50 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]"
+                  onClick={reset}
+                >
+                  RESET
+                </button>
+                { clicked ? <CgpaResult score={inputText}/> : null }
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+
+}
+
+export default Sem6
--- src/components/Spga.jsx
+++ src/components/Spga.jsx
@@ -1,90 +1,170 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
+import { Link } from 'react-router-dom'
+import Results from './Sheet.jsx';
 
 const Spga = () => {
-  const [inputText,setinputText] = useState([
-    {
-      sub1:"",
-      sub2:"",
-      sub3:"",
-      sub4:"",
-      sub5:"",
-      sub6:"",
-      sub7:"",
-      sub8:""
-    },
 
-  ]);
-  
-  const modchange = (e)=>{
-    console.log(e.target.value);
+  const [inputText, setinputText] = useState(
+    {
+      sub1: null,
+      sub2: null,
+      sub3: null,
+      sub4: null,
+      sub5: null,
+      sub6: null,
+      sub7: null,
+      sub8: null,
+    });
+
+
+  const modchange = (e) => {
     const value = e.target.value;
     const name = e.target.name;
-    setinputText((old)=>{
-      return {...old,[name]:value}
-    })
-  }
-  const modSubmit = (e)=>{
-    e.preventDefault();
-    console.log(inputText);
-  }
-  return (
-    
-  <div className='absolute top-0 left-0 min-h-fit min-w-full container mt-10'  >
-      <form action="" onSubmit={modSubmit}>
-        
-      <div className="inputs ">
-      <div className="left flex flex-col gap-y-8 text-white min-h-screen min-w-full p-9  font-mono">
-        <label htmlFor="">
-          sub1
-        <input type="text" inputmode="numeric" name="sub1" id=""  className='m-3 rounded-lg text-black' placeholder='Marks' onChange={modchange}/>
-        </label>
-        <label htmlFor="">
-          sub2
-          <input type="text" inputmode="numeric" name="sub1" id="" className=' rounded-lg mx-3  text-black' placeholder='Marks' onChange={modchange} />
-        </label>
-        <label htmlFor="">
-          sub3
-          <input type="text" inputmode="numeric" name="sub1" id="" className=' rounded-lg mx-3  text-black'
-          placeholder='Marks' onChange={modchange} />
-        </label>
-        <label htmlFor="">
-          sub4
-          <input type="text" inputmode="numeric" name="sub1" id=""className=' rounded-lg mx-3  text-black' placeholder='Marks' onChange={modchange} />
-        </label>
-        <label htmlFor="">
-          sub5
-          <input type="text" inputmode="numeric" name="sub1" id=""className=' rounded-lg mx-3 text-black' placeholder='Marks' onChange={modchange} />
-        </label>
-        <label htmlFor="">
-          sub6
-          <input type="text" inputmode="numeric" name="sub1" id=""className=' rounded-lg mx-3 text-black' placeholder='Marks'onChange={modchange}  />
-        </label>
-        <label htmlFor="">
-          sub7
-          <input type="text" inputmode="numeric" name="sub1" id=""className=' rounded-lg mx-3  text-black' placeholder='Marks' onChange={modchange}  />
-        </label>
-        <label htmlFor="">
-          sub6
-          <input type="text" inputmode="numeric" name="sub1" id=""className=' rounded-lg mx-3  text-black' placeholder='Marks' onChange={modchange}  />
-        </label>
-<div className="last-btn flex justify-center items-center gap-3  ">
-<button
-  type="submit" onClick={modSubmit}
-  className=" w-24  flex justify-center rounded bg-neutral-50 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]">
-  SUBMIT
-</button>
-        <button
-  type="reset"
-  className=" w-24  flex justify-center rounded bg-neutral-50 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]">
-  Reset
-</button>
-</div>
-      </div>
-      </div>
-      </form>
-  </div>
-  )
-}
+    setinputText((old) => {
+      return { ...old, [name]: Number(value) };
+    });
+    console.log(inputText)
+  };
 
-export default Spga
+  const [clicked,setclick]=useState(false);
+
+  const modSubmit = (e) => {
+    e.preventDefault();
+    setclick(true)
+  };
+
+  const reset = (e) => {
+    setinputText( 
+      {
+        sub1: null,
+        sub2: null,
+        sub3: null,
+        sub4: null,
+        sub5: null,
+        sub6: null,
+        sub7: null,
+        sub8: null,
+      }
+    )
+    setclick(false)
+  }
+
+
+  return (
+    <div className="absolute top-0 left-0 min-h-fit min-w-full container mt-10">
+      <form action="" onSubmit={modSubmit}>
+        <div className="inputs ">
+          <div className="left flex flex-col gap-y-8 text-white min-h-screen min-w-full p-9  font-mono">
+            <label htmlFor="">
+              sub1
+              <input
+                type="numbenull"
+                name="sub1"
+                value={inputText.sub1}
+                className="m-3 rounded-lg text-black"
+                placeholder="Marks"
+                onChange={modchange}
+              />
+            </label>
+            <label htmlFor="">
+              sub2
+              <input
+                type="number"
+                name="sub2"
+                value={inputText.sub2}
+                className=" rounded-lg mx-3  text-black"
+                placeholder="Marks"
+                onChange={modchange}
+              />
+            </label>
+            <label htmlFor="">
+              sub3
+              <input
+                type="number"
+                name="sub3"
+                value={inputText.sub3}
+                className=" rounded-lg mx-3  text-black"
+                placeholder="Marks"
+                onChange={modchange}
+              />
+            </label>
+            <label htmlFor="">
+              sub4
+              <input
+                type="number"
+                name="sub4"
+                value={inputText.sub4}
+                className=" rounded-lg mx-3  text-black"
+                placeholder="Marks"
+                onChange={modchange}
+              />
+            </label>
+            <label htmlFor="">
+              sub5
+              <input
+                type="number"
+                name="sub5"
+                value={inputText.sub5}
+                className=" rounded-lg mx-3 text-black"
+                placeholder="Marks"
+                onChange={modchange}
+              />
+            </label>
+            <label htmlFor="">
+              sub6
+              <input
+                type="number"
+                name="sub6"
+              value={inputText.sub6}
+                className=" rounded-lg mx-3 text-black"
+                placeholder="Marks"
+                onChange={modchange}
+              />
+            </label>
+            <label htmlFor="">
+              sub7
+              <input
+                type="number"
+                name="sub7"
+                value={inputText.sub7}
+                className=" rounded-lg mx-3  text-black"
+                placeholder="Marks"
+                onChange={modchange}
+              />
+            </label>
+            <label htmlFor="">
+              sub8
+              <input
+                type="number"
+                name="sub8"
+                value={Number(inputText.sub8)}
+                className=" rounded-lg mx-3  text-black"
+                placeholder="Marks"
+                onChange={modchange}
+              />
+            </label>
+            <div className="last-btn flex justify-center items-center gap-3  ">
+            <button
+                type="submit"
+                onClick={modSubmit}
+                className=" w-24  flex justify-center rounded bg-neutral-50 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]"
+              >
+                SUBMIT
+              </button>
+              <button
+                type="reset"
+                className=" w-24  flex justify-center rounded bg-neutral-50 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]"
+                onClick={reset}
+              >
+                RESET
+              </button>
+              { clicked ? <Results score={inputText}/> : null }
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Spga;
