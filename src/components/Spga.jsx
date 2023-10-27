db393dace8814bf4e4c3a3af7b6fe72d5009ce82
Nikhil AL <nikhilal.2703@gmail.com>
2023-10-27 22:34:42 +0530

    Update Spga.jsx

--- src/components/Spga.jsx
+++ src/components/Spga.jsx
@@ -4,11 +4,7 @@ import Results from "./Sheet.jsx";
 import MyModel from "./MyModel.jsx";
 
 const Spga = () => {
-  const [showMyModal, setshowMyModel] = useState(false);
-
-  const handleOnClose = () => {
-    setshowMyModel(false);
-  };
+  
   const [inputText, setinputText] = useState({
     sub1: null,
     sub2: null,
@@ -165,9 +161,9 @@ const Spga = () => {
           <button
             type="submit"
             // onClick={modSubmit}
-            onClick={() => {
-              modSubmit, setshowMyModel(true);
-            }}
+            onClick={
+              modSubmit
+            }
             className=" w-24  flex justify-center rounded bg-neutral-50 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] m-5  "
           >
             SUBMIT
@@ -181,13 +177,13 @@ const Spga = () => {
           </button>
           {clicked ? (
             <Results
-              score={inputText}
-              onClose={handleOnClose}
-              visible={showMyModal}
+              score={inputText,clicked}
+              
+              
             />
           ) : null}
         </div>
-      </form>{" "}
+      </form>
     </div>
   );
 };
