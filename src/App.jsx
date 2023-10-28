6998f25005df06d04602a4ca9d0a9e17a61f38ce
rockygeekz <rakesh.s552004@gmail.com>
2023-10-28 17:27:56 +0530

    changed reactrouter to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header.jsx";
 import Home from "./components/Home.jsx";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   createBrowserRouter,
   RouterProvider,
   Routes,
